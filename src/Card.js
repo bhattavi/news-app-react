4bb2b811715316cc59533b0840cd5d8820f5114c
abhinav <bhattabhinav@github.com>
2021-03-17 16:37:44 -0400

    trending cards added

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card({news, index}) {
     return (
         <div>
-            <h1>this is card</h1>
+            <img style={{width: "100vw", height: "60vh"}} src= {news[index].urlToImage} alt=""/>
+            <h1>{news[index].title}</h1>
+            <h2>{news[index].author}</h2>
+            <h3>{news[index].source.name}</h3>
+            <p>{news[index].description}</p>
         </div>
     )
 }
--- src/Trending.js
+++ src/Trending.js
@@ -1,16 +1,60 @@
-
+import React, {useState, useEffect} from 'react'
 import Navbar from './Navbar'
 import Card from './Card'
+import axios from 'axios'
 const API_KEY = process.env.REACT_APP_YOUR_API_KEY_NAME;
 
-function Trending() {
-  
-  return (
-   <div>
-     <Navbar></Navbar>
-<h1>this is trending</h1>
-     <Card></Card>
 
+
+function Trending() {
+
+    const[trending, setTrending] = useState([]);
+    const[index, setIndex] = useState(0);
+ 
+  
+    const fetchTrending = async() => {
+        try {
+           
+            const {data} = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+            setTrending(data.articles)
+            //console.log(data.articles)
+        } catch (error) {
+            
+        }
+       
+    }
+    useEffect(() => {
+        fetchTrending();
+        
+    },[])
+
+    const increment = () =>{
+        let currIndex = index;
+        currIndex++;
+        setIndex(currIndex);
+    }
+    const decrement = () =>{
+        if(index > 0){
+        let currIndex = index;
+        currIndex--;
+        setIndex(currIndex);
+        }else setIndex(0);
+    }
+    const btnStyle = {
+        display: "flex",
+        justifyContent: "space-around"
+    }
+  return (
+     
+   <div>
+        {console.log(index)}
+     <Navbar></Navbar>
+     {trending.length > 1 &&(<Card news ={trending} index = {index}></Card>)}
+    
+     <div style = {btnStyle}>
+        <button onClick={decrement}>Back</button>
+        <button onClick={increment}>Next</button>
+        </div>
    </div>
   );
 }
