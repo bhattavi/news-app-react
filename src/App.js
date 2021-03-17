ef291a9880727437d03346392b59a7e88c241f8c
abhinav <bhattabhinav@github.com>
2021-03-17 15:11:40 -0400

    .env file added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_YOUR_API_KEY_NAME;
 function App() {
+  console.log(API_KEY);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div></div>
   );
 }
 
